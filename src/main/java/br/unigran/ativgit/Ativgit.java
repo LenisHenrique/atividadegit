0b6aa1d2d2e25b03c63fae65d84905c9886b5be6
Henrique <henriquelenis11@gmail.com>
2024-04-25 20:24:19 -0400

    Commit um

---
+++ src/main/java/br/unigran/ativgit/Ativgit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.unigran.ativgit;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Ativgit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
